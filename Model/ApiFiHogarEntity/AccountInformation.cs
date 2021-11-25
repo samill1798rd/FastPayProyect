c8d6180bc5cb8147960153da03f7db69bad54365
Samill <samill.rivasrd@gmail.com>
2021-11-25 00:48:45 -0400

    Refactory of some many class in folder Model .

--- Model/ApiFiHogarEntity/AccountInformation.cs
+++ Model/ApiFiHogarEntity/AccountInformation.cs
@@ -1,28 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Model.ApiFiHogarEntity
+﻿namespace Model.ApiFiHogarEntity
 {
     public class AccountInformation
     {
         public Header Header { get; set; }
         public Data Data { get; set; }
     }
-
-
-
-    //public class Data
-    //{
-    //    public List<Account> Account { get; set; }
-    //}
-
-    //public class Root
-    //{
-    //    public Header header { get; set; }
-    //    public Data Data { get; set; }
-    //}
-
-
 
 }
--- Model/ApiFiHogarEntity/SecondAccessToken.cs
+++ Model/ApiFiHogarEntity/SecondAccessToken.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Model.ApiFiHogarEntity
+﻿namespace Model.ApiFiHogarEntity
 {
     public class SecondAccessToken
     {
